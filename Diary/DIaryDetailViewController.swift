116ffb7c5a9150250b755a11e9045cddf9f4643d
TLtlbo <js1436kt@gmail.com>
2023-07-21 00:26:57 +0900

    일기 작성 뷰 구현 및 일기 상세 뷰 구현

---
+++ Diary/DIaryDetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DIaryDetailViewController.swift
+//  Diary
+//
+//  Created by 박진성 on 2023/07/21.
+//
+
+import UIKit
+
+class DIaryDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
---
+++ Diary/WriteDiaryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  WriteDiaryViewController.swift
+//  Diary
+//
+//  Created by 박진성 on 2023/07/21.
+//
+
+import UIKit
+
+class WriteDiaryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
