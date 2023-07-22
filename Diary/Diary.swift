dfb2449336e7f0005d98b8eb349385e84b68d01f
TLtlbo <js1436kt@gmail.com>
2023-07-22 20:51:01 +0900

    작성된 일기 조회 뷰에 콜렉션 뷰를 통한 일기 조회 기능 구현 및 UserDefaults를 이용한 저장 및 로드 구현

---
+++ Diary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  Diary
+//
+//  Created by 박진성 on 2023/07/22.
+//
+
+import Foundation
+
+struct Diary {
+    var title : String
+    var contents : String
+    var date : Date
+    var isStar : Bool
+}
